fa62bc2bb629f1f24eb979b06a028c4a13c9d692
programfive <kennedysuedavid24@gmail.com>
2023-07-31 20:47:46 -0400

    code qr complete

--- script/index.js
+++ script/index.js
@@ -1,23 +1,21 @@
-const $imagen = document.querySelector("#codigo");
-const $boton = document.querySelector("#btnDescargar");
-const $guardar = document.querySelector('#guardar');
+const $image = document.querySelector("#image");
+const $download = document.querySelector("#download");
+const $save = document.querySelector('#save');
 const $search = document.querySelector('#search');
 const $content = document.querySelector('#content');
 
-$boton.onclick = () => {
-  const enlace = document.createElement("a");
-  enlace.href = $imagen.src;
-  enlace.download = "Código QR generado";
-  enlace.click();
+$download.onclick = () => {
+  let link = document.createElement("a");
+  link.href = $image.src;
+  link.download = "Código QR generado";
+  link.click();
 };
-
-$guardar.onclick = () => {
+$save.onclick = () => {
   if ($search.value) {
     $content.classList.remove('hidden'); 
     $content.classList.add('opacity-100', 'block'); 
-
     new QRious({
-      element: $imagen,
+      element: $image,
       value: $search.value,
       size: 500,
       backgroundAlpha: 0,
@@ -26,7 +24,6 @@ $guardar.onclick = () => {
     });
   }else{
     $content.classList.remove('block'); 
-    $content.classList.add('opacity-100', 'hidden'); 
-
+    $content.classList.add('opacity-100','hidden'); 
   }
 };
