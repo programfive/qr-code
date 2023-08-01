46a6449b9b028a1d4b2e59990c52dcc7907cf85b
programfive <kennedysuedavid24@gmail.com>
2023-07-31 22:25:03 -0400

    validation input

--- script/index.js
+++ script/index.js
@@ -3,6 +3,8 @@ const $download = document.querySelector("#download");
 const $save = document.querySelector('#save');
 const $search = document.querySelector('#search');
 const $content = document.querySelector('#content');
+const $error=document.querySelector('#error');
+
 
 $download.onclick = () => {
   let link = document.createElement("a");
@@ -11,19 +13,25 @@ $download.onclick = () => {
   link.click();
 };
 $save.onclick = () => {
-  if ($search.value) {
-    $content.classList.remove('hidden'); 
-    $content.classList.add('opacity-100', 'block'); 
+  const usuarioInput = $search.value;
+  try {
+    const url = new URL(usuarioInput);
+
+    $content.classList.remove('hidden');
+    $content.classList.add('opacity-100', 'block');
     new QRious({
       element: $image,
-      value: $search.value,
+      value: url.href,
       size: 500,
       backgroundAlpha: 0,
       foreground: "#334155",
       level: "H",
     });
-  }else{
-    $content.classList.remove('block'); 
-    $content.classList.add('opacity-100','hidden'); 
+    $error.classList.add('hidden');
+  } catch (error) {
+    $content.classList.remove('block');
+    $content.classList.add('opacity-100', 'hidden');
+    $error.textContent = "URL inválida";
+    $error.classList.remove('hidden');
   }
 };
